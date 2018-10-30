0b37c9907ff904e435f2b3831e3b8709139993b4
Brian Bartholomew <bartholomewbd@gmail.com>
2018-10-30 19:26:57 -0400

    Initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = ({ robots }) => robots.map((user, i) => <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} /> )
+
+export default CardList
---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Grimes',
+    username: 'Bros',
+    email:'awesome@group.com'
+  },
+  {
+    id: 2,
+    name: 'Suzie Gram',
+    username: 'Hotness',
+    email:'myemail123@mail.com'
+  },
+  {
+    id: 3,
+    name: 'Christian Fluffy',
+    username: 'Fluffycat',
+    email:'cfluf@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Bobby Yolo',
+    username: 'broyolo',
+    email:'bobbysemail@small.biz'
+  },
+  {
+    id: 5,
+    name: 'Hustle Reyes',
+    username: 'thehustle',
+    email:'alwayshustling@email.com'
+  },
+  {
+    id: 6,
+    name: 'Brian Jackson',
+    username: 'bjax',
+    email:'bjax@cable.com'
+  },
+  {
+    id: 7,
+    name: 'Dave Baxter',
+    username: 'dave',
+    email:'dman@yahoo.com'
+  },
+  {
+    id: 8,
+    name: 'Maria Gonzolez',
+    username: 'mamagonz',
+    email:'mgonz@hotmail.com'
+  },
+  {
+    id: 9,
+    name: 'Sam Honda',
+    username: 'sahot',
+    email:'sam.honda@cable.com'
+  },
+  {
+    id: 10,
+    name: 'Mark James',
+    username: 'markymark',
+    email:'markymarx@myhostingcompany.com'
+  }
+
+]
