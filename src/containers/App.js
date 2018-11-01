bada642cc00128707783f67c1c20b9bfc07636dd
Brian Bartholomew <bartholomewbd@gmail.com>
2018-11-01 17:12:09 -0400

    added error boundry to learn about error catching

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
 
 
@@ -32,17 +33,17 @@ class App extends Component {
     })
     return !robots.length ?
       <h2>LOADING</h2> :
-    (
-   
+    ( 
       <div className ='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots}/>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundry>
           </Scroll>
       </div>
     );
-    
     }
   
 
